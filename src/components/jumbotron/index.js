4591f51f7cab45271e422a7d1682b8cc00a2435d
Jose Manuel <jmdcarq89@gmail.com>
2021-03-15 20:55:33 +0100

    jumbotron styles

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Container, Inner } from './styles/jumbotron';
+import { Container, Inner, Item, Pane, Title, SubTitle, Image } from './styles/jumbotron';
 
 export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
-        <Inner direction={direction}>
-            {children}
-        </Inner>
-    )
-}
+      <Item {...restProps}>
+        <Inner direction={direction}>{children}</Inner>
+      </Item>
+    );
+  }
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
