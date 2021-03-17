33825d85709f4bc5a46bb4366848151faf94a5b3
Jose Manuel <jmdcarq89@gmail.com>
2021-03-17 22:36:48 +0100

    firebase data

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,15 +1,15 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { seedDatabase } from '../seed';
 
 const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: "AIzaSyA4dlucPpRw4taoHrTroDhenic_DOy4fhI",
+    authDomain: "netflixclone-e5769.firebaseapp.com",
+    projectId: "netflixclone-e5769",
+    storageBucket: "netflixclone-e5769.appspot.com",
+    messagingSenderId: "891437577755",
+    appId: "1:891437577755:web:5f8fd2c285c2ae1487eabe"
   };
 
   const firebase = Firebase.initializeApp(config);
