65645e428b7cc2eb8ce139bef36d43a3f2b2fd0c
Jose Manuel <jmdcarq89@gmail.com>
2021-03-17 22:24:56 +0100

    firebase config

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+  };
+
+  const firebase = Firebase.initializeApp(config);
+
+  export { firebase };
