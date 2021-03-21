c795213f88208d9659b2c5aee3ff4a19657f6e31
Jose Manuel <jmdcarq89@gmail.com>
2021-03-21 20:01:48 +0100

    SignIn return

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -35,6 +35,38 @@ export default function SignIn() {
 
     return (
         <>
+        <HeaderContainer>
+            <Form>
+            <Form.Title>Sign In</Form.Title>
+            {error && <Form.Error data-testid="error">{error}</Form.Error>}
+
+            <Form.Base onSubmit={handleSignin} method="POST">
+                <Form.Input
+                placeholder="Email address"
+                value={emailAddress}
+                onChange={({ target }) => setEmailAddress(target.value)}
+                />
+                <Form.Input
+                type="password"
+                value={password}
+                autoComplete="off"
+                placeholder="Password"
+                onChange={({ target }) => setPassword(target.value)}
+                />
+                <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
+                Sign In
+                </Form.Submit>
+            </Form.Base>
+
+            <Form.Text>
+                New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+                This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+            </Form.TextSmall>
+            </Form>
+        </HeaderContainer>
+        <FooterContainer />
         </>
     );
 
