56125dfb6fdd950f670c4141256967462f4d2d89
Jose Manuel <jmdcarq89@gmail.com>
2021-03-21 19:59:42 +0100

    SignIn logic

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { FooterContainer } from '../containers/footer';
-import { HeaderContainer } from '../containers/header';
 
-export default function Signin() {
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
+
+export default function SignIn() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+  
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+  
+    const isInvalid = password === '' || emailAddress === '';
+  
+    const handleSignin = (event) => {
+      event.preventDefault();
+  
+      return firebase
+        .auth()
+        .signInWithEmailAndPassword(emailAddress, password)
+        .then(() => {
+          history.push(ROUTES.BROWSE);
+        })
+        .catch((error) => {
+          setEmailAddress('');
+          setPassword('');
+          setError(error.message);
+        });
+    };
+
     return (
-    <>
-        <HeaderContainer>Hello from the sign ip!</HeaderContainer>
-        <FooterContainer/>
-    </>
+        <>
+        </>
     );
+
 }
