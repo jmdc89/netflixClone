6971d4c171b1d2c27790bf7885f84af91f034ec9
Jose Manuel <jmdcarq89@gmail.com>
2021-03-24 21:24:54 +0100

    Browse - imports-containers

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import Fuse from 'fuse.js';
+import { Card, Header, Loading, Player } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
+import { FirebaseContext } from '../context/firebase';
+import { SelectProfileContainer } from './profiles';
+import { FooterContainer } from './footer';
 
 export function BrowseContainer({slides}) {
-    return <p>Hello!</p>
+    
 }
